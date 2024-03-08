dc1b32fb72927afa7f51ed005c75f281cd7a070e
Sh Raj <shaswatraj3@gmail.com>
2024-03-08 13:42:17 +0530

    commentviewer

--- community/script.js
+++ community/script.js
@@ -1,3 +1,5 @@
+let commentviewer = document.querySelector(".commentviewer") ;
+
 function showSection(section) {
     // Remove 'active' class from all sections
     document.querySelectorAll('.playground .layout').forEach(function(el) {
@@ -9,4 +11,24 @@ function showSection(section) {
 
     // Scroll the selected section into view
     document.querySelector(`.${section}`).scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+}
+
+document.querySelectorAll(".post .comment").forEach((e)=>{
+    e.addEventListener("click",(e)=>showComments(e))
+})
+let showComments = (e) => {
+    let postId = e.target.parentElement.parentElement.parentElement.id;
+    // commentviewer.style.display = "block";
+    let html = `
+    <div class="commentviewer">
+                        <div class="comment">
+                            <span class="author"><img src="../assets/logo.png" alt=""><span><span class="tomato">@</span>sh20raj</span></span>
+                            <span class="content">Hii, It's Just Wonderful Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, ipsam. Quia incidunt nam, natus illo possimus eligendi hic animi unde asperiores repellendus eaque nostrum ea quidem tempora. Ea, maiores totam!</span>
+                        </div>
+                    </div>
+                    `;
+    commentviewer.classList.toggle("visible");
+    commentviewer.insertAdjacentHTML = postId;
+
+
 }
