0c22efb9bd0ea8f4d78e77ab6dc639a454434d08
Sh Raj <shaswatraj3@gmail.com>
2024-03-08 13:55:43 +0530

    comment section

--- community/script.js
+++ community/script.js
@@ -1,4 +1,3 @@
-let commentviewer = document.querySelector(".commentviewer") ;
 
 function showSection(section) {
     // Remove 'active' class from all sections
@@ -17,18 +16,23 @@ document.querySelectorAll(".post .comment").forEach((e)=>{
     e.addEventListener("click",(e)=>showComments(e))
 })
 let showComments = (e) => {
+    e.target.classList.toggle("tomato")
     let postId = e.target.parentElement.parentElement.parentElement.id;
+    let post = e.target.parentElement.parentElement.parentElement;
     // commentviewer.style.display = "block";
     let html = `
-    <div class="commentviewer">
+    <div class="commentviewer ">
                         <div class="comment">
                             <span class="author"><img src="../assets/logo.png" alt=""><span><span class="tomato">@</span>sh20raj</span></span>
                             <span class="content">Hii, It's Just Wonderful Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, ipsam. Quia incidunt nam, natus illo possimus eligendi hic animi unde asperiores repellendus eaque nostrum ea quidem tempora. Ea, maiores totam!</span>
                         </div>
                     </div>
                     `;
+    post.insertAdjacentHTML("beforeend",html);
+    let commentviewer = post.querySelector(".commentviewer") ;
+    console.log("cv",commentviewer);
     commentviewer.classList.toggle("visible");
     commentviewer.insertAdjacentHTML = postId;
-
+    console.log(post);
 
 }
